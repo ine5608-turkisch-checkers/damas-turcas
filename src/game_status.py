f431ceb712213737c801bb876867936c0bfe8b84
Daniel Do Amaral <danieldoamaral2134@gmail.com>
2025-06-24 20:09:43 -0300

    Dani

--- src/game_status.py
+++ src/game_status.py
@@ -1,9 +1,9 @@
 from enum import Enum
 
 class GameStatus(Enum):
-    NO_MATCH = 1
-    FINISHED = 2
-    WAITING_LOCAL_MOVE = 3
-    OCURRING_LOCAL_MOVE = 4
-    WAITING_REMOTE_MOVE = 5
-    ABANDONED = 6
+    NO_MATCH = 1              # Partida não iniciada
+    FINISHED = 2              # Partida finalizada
+    WAITING_LOCAL_MOVE = 3    # Esperando jogada do jogador local
+    OCCURRING_LOCAL_MOVE = 4  # Jogada local em andamento
+    WAITING_REMOTE_MOVE = 5   # Esperando jogada do jogador remoto
+    ABANDONED = 6             # Partida abandonada pelo oponente
