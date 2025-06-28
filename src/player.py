e2dc03ea43948e5f08e833159cb03879531d7741
felipedelduqui <felipedelduqui@hotmail.com>
2025-06-28 19:03:36 -0300

    Atualização de classes. Organização do Board e Player

--- src/player.py
+++ src/player.py
@@ -5,36 +5,29 @@ from piece import Piece
 MAX_NUMBER_OF_PIECES = 16
 
 class Player():
-    def __init__(self, id: int, name: str, is_black: bool, is_its_turn: bool, pieces: Optional[List[Piece]] = None) -> None:
+    def __init__(self, id: int, name: str, is_black: bool, is_its_turn: bool) -> None:
         self._id: int = id
         self._name:str = name
         self._is_black:bool = is_black
         self._is_its_turn: bool = is_its_turn
         self._is_winner: bool = False
-        if pieces is None:
-            self._pieces = []
-        else:
-            if not all(isinstance(p, Piece) for p in pieces):
-                raise TypeError("All pieces must be instances of Piece.")
-            if len(pieces) != MAX_NUMBER_OF_PIECES:
-                raise ValueError(f"Player must start with exactly {MAX_NUMBER_OF_PIECES} pieces.")
-            self._pieces = pieces
+        self._pieces: List[Piece] = [Piece(self) for _ in range(MAX_NUMBER_OF_PIECES)] #Diagrama de sequência Initialize: Player instancia as suas peças sem posição 
 
     @property
     def id(self) -> int:
         return self._id
 
     @id.setter
-    def id(self, value: int) -> None:
-        self._id = value
+    def id(self, id: int) -> None:
+        self._id = id
 
     @property
     def name(self) -> str:
         return self._name
 
     @name.setter
-    def name(self, value: str) -> None:
-        self._name = value
+    def name(self, name: str) -> None:
+        self._name = name
 
     @property
     def is_black(self) -> bool:
@@ -66,11 +59,3 @@ class Player():
     @property
     def pieces(self) -> List[Piece]:
         return self._pieces
-
-    @pieces.setter
-    def pieces(self, new_pieces: List[Piece]) -> None:
-        if len(new_pieces) != MAX_NUMBER_OF_PIECES:
-            raise ValueError(f"Player must start with exactly {MAX_NUMBER_OF_PIECES} pieces.")
-        if not all(isinstance(p, Piece) for p in new_pieces):
-            raise TypeError("All pieces must be instances of Piece.")
-        self._pieces = new_pieces
